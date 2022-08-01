9efa0c7c6dcf3a517ee7680f4bc24d659411078d
TriumphErmola <xramovic@yandex.ru>
2022-08-01 20:23:12 +0600

    List Veterenar adding

--- Pet-Clinic-master/pet-clinic-web/src/main/java/springguru/petclinic/controllers/VeterenarController.java
+++ Pet-Clinic-master/pet-clinic-web/src/main/java/springguru/petclinic/controllers/VeterenarController.java
@@ -1,13 +1,23 @@
 package springguru.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import springguru.petclinic.service.VeterenarServiceImpl;
 
 @Controller
 public class VeterenarController {
 
+    private final VeterenarServiceImpl veterenarService;
+
+    public VeterenarController(VeterenarServiceImpl veterenarService) {
+        this.veterenarService = veterenarService;
+    }
+
     @RequestMapping({"/veterenar/index","/veterenar","/veterenar/index.html "})
-    public String listOfVeterenarov(){
+    public String listOfVeterenarov(Model model){
+        model.addAttribute("veterenar",veterenarService.findAll());
+
         return "veterenar/index";
     }
 
