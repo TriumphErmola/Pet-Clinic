dc7fcd15ec0557e658e51260c5dce5cbfda724b5
TriumphErmola <xramovic@yandex.ru>
2022-08-10 21:32:02 +0600

    25

--- Pet-Clinic-master/pet-clinic-data/src/main/java/springguru/petclinic/model/MasterOfAnimal.java
+++ Pet-Clinic-master/pet-clinic-data/src/main/java/springguru/petclinic/model/MasterOfAnimal.java
@@ -4,10 +4,33 @@ import java.util.Set;
 
 public class MasterOfAnimal extends Human{
 
+    private String addres;
+    private String city;
+    private String telephone;
     private Set<HomeAnimal> homeAnimalSet;
 
+    public String getAddres() {
+        return addres;
+    }
 
-    public MasterOfAnimal() {
+    public void setAddres(String addres) {
+        this.addres = addres;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
     }
 
     public Set<HomeAnimal> getHomeAnimalSet() {
